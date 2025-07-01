dbc1b6917f8fdd2bdd75ed52e2022f69ef0fb23f
Moises Protazio <moisesprotazio@gmail.com>
2025-07-01 11:55:40 -0300

    my first graph

--- matplotlib_lesson.py
+++ matplotlib_lesson.py
@@ -1,14 +1,23 @@
 import matplotlib
+matplotlib.use('Qt5Agg') # uses pyqt to show
+import matplotlib.pyplot as plt
 
 #For this file I'll study matplotlib from https://matplotlib.org/stable/tutorials/index.html
+
+def first_plot():
+    plt.plot([2, 2, 2, 4]) #create a line on the plot through the points
+    plt.plot([2, 3, 4, 5]) # all points are based on y
+
+    plt.plot([2, 2, 2, 4], [1, 2, 3,4]) # the first array is the x value, and the second is the y value
+    #the y starts with the smallest value of the plots
+    plt.ylabel('some numbers') #the y label
+    plt.xlabel('other numbers')  #the x label
+    plt.show() #shows the plot
+
 
 
 
 if __name__ == '__main__':
     print(matplotlib.__version__)
-    fun_list = []
-
-    for fun in fun_list:
-        fun()
-        print("-" * 43)
+    first_plot()
 
