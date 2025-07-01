b0acf1a90f00ccaa6e084d3caba04833fa998a98
Moises Protazio <moisesprotazio@gmail.com>
2025-06-30 22:02:17 -0300

    Start studyng matplotlib

---
+++ matplotlib_lesson.py
@@ -0,0 +1,14 @@
+import matplotlib
+
+#For this file I'll study matplotlib from https://matplotlib.org/stable/tutorials/index.html
+
+
+
+if __name__ == '__main__':
+    print(matplotlib.__version__)
+    fun_list = []
+
+    for fun in fun_list:
+        fun()
+        print("-" * 43)
+
